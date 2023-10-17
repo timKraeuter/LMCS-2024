bfbe810f456eac3e826729514fb773b6c8b12fac
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-10-17 09:44:25 +0200

    Create scripts dir

---
+++ artifacts/experiment/results/scripts/README.md
@@ -0,0 +1,7 @@
+```bash
+py plot_HOT_scalability.py ./../HOTScalability_stats.json
+```
+
+```bash
+py plot_SGen_scalability.py ./../SGenScalability_stats.json
+```
---
+++ artifacts/experiment/results/scripts/plot_HOT_scalability.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+"""Generate the scalability plot for the article."""
+
+import argparse
+import json
+import numpy as np
+import matplotlib.pyplot as plt
+
+parser = argparse.ArgumentParser(description=__doc__)
+parser.add_argument("file", help="JSON file with benchmark results")
+
+args = parser.parse_args()
+
+with open(args.file) as f:
+    results = json.load(f)["results"]
+
+all_means = [b["mean"] for b in results]
+plt.plot(list(range(1,301)), all_means)
+
+plt.title("HOT scalability")
+plt.xlabel("Blocks per BPMN model")
+plt.ylabel("Runtime in s")
+
+plt.show()
