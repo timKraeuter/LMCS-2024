ebf2ff77e84440da29a6090461af670aaebdfe5c
Tim Kräuter <tim.kraeuterr@gmail.com>
2023-10-18 10:12:10 +0200

    Add linear regression and remove unused scripts

--- artifacts/experiment/results/scripts/plot_HOT_scalability.py
+++ artifacts/experiment/results/scripts/plot_HOT_scalability.py
@@ -4,7 +4,7 @@
 
 import argparse
 import json
-import numpy as np
+from scipy import stats
 import matplotlib.pyplot as plt
 
 parser = argparse.ArgumentParser(description=__doc__)
@@ -16,10 +16,23 @@ with open(args.file) as f:
     results = json.load(f)["results"]
 
 all_means = [b["mean"] for b in results]
-plt.plot(list(range(1,301)), all_means)
+x = list(range(1, 301))
+
+plt.plot(x, all_means, label="data")
+
+# Linear regression
+slope, intercept, r, p, std_err = stats.linregress(x, all_means)
+
+
+def myfunc(x):
+    return slope * x + intercept
+
+
+mymodel = list(map(myfunc, x))
+plt.plot(x, mymodel, label="linregress")
 
 # plt.title("HOT scalability")
 plt.xlabel("Blocks per BPMN model")
 plt.ylabel("Runtime in s")
-
+plt.legend()
 plt.show()
